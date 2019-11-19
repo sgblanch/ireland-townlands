51f71dc60ad009e7eba7e467d15eadde788a213b
Steven Blanchard <sgblanch@uncc.edu>
2019-11-18 21:44:44 -0500

    Fix bug in mkdir; prettify maps

--- gen-maps.R
+++ gen-maps.R
@@ -8,7 +8,7 @@ library(tmap)
 townlands <- st_read(file.path("data", "townlands.shp"), options = "ENCODING=UTF-8")
 
 if(!dir.exists("maps"))
-  dir.create("maps", county)
+  dir.create("maps")
 
 #for (county in levels(townlands$CO_NAME)) {
 for (county in c("Kilkenny", "Offaly", "Tipperary", "Waterford")) {
@@ -20,7 +20,12 @@ for (county in c("Kilkenny", "Offaly", "Tipperary", "Waterford")) {
   for (parish in levels(county.shp$CP_NAME)) {
     parish.shp <- county.shp[county.shp$CP_NAME==parish, ]
     parish.shp$NAME_TAG <- droplevels(parish.shp$NAME_TAG)
-    parish.map <- tm_shape(parish.shp) + tm_fill("NAME_TAG") + tm_text("NAME_TAG")
+    parish.map <- tm_shape(parish.shp)
+    parish.map <- parish.map + tm_polygons("MAP_COLORS", ncols = 5)
+    parish.map <- parish.map + tm_text("NAME_TAG") # auto.placement = TRUE
+    parish.map <- parish.map + tm_legend(legend.show = FALSE)
+    parish.map <- parish.map + tm_scale_bar()
+    #parish.map <- parish.map + tm_compass()
     tmap_save(parish.map, file.path("maps", county, paste0(parish, ".pdf")), width=10, height=10, units = "in")
   }
 }
