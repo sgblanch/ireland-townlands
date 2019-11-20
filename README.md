210fc49d405f9606e8ca67574cdff5e1f2d7dadb
Steven Blanchard <sgblanch@uncc.edu>
2019-11-20 11:10:53 -0500

    Use civil parish data to locate unlabelled townlands

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # ireland-townlands
 
-- Download map data from https://www.townlands.ie/page/download/
+- Download shapefiles for both townlands and civil parishes from https://www.townlands.ie/page/download/
+- Create 'data' directory at top level of this repo and unzip shapefiles there
 - Install R from https://www.r-project.org/
 - Install RStudio from https://rstudio.com/products/rstudio/
-
-Note: You may need to install packrat manually
-
-```R
-install.packages("packrat")
-```
+- Open Project in RStudio and follow prompts to install R dependencies
+- Source `gen-maps.R` in RStudio.  Maps will be created in a `maps` directory at top level of repo
--- gen-maps.R
+++ gen-maps.R
@@ -1,31 +1,56 @@
 #!/usr/bin/env Rscript --vanilla
 
+library(dplyr)
 library(sf)
 library(tmap)
 
-# https://www.townlands.ie/static/downloads/townlands.zip
+# https://www.townlands.ie/static/downloads/civil_parishes.zip
+parishes <- st_read(file.path("data", "civil_parishes.shp"), options = "ENCODING=UTF-8")
 
+# https://www.townlands.ie/static/downloads/townlands.zip
 townlands <- st_read(file.path("data", "townlands.shp"), options = "ENCODING=UTF-8")
 
-if(!dir.exists("maps"))
-  dir.create("maps")
+# dump parish data to CSV
+parishes.out <- st_drop_geometry(parishes)
+parishes.out <- parishes.out[, c("NAME_TAG", "NAME_GA", "NAME_EN", "ALT_NAME", "ALT_NAME_G", "CO_NAMES", "ATTRIBUTIO")]
+write.csv( parishes.out, file.path("data", "civil_parishes.csv"), fileEncoding = "UTF-8")
 
-#for (county in levels(townlands$CO_NAME)) {
-for (county in c("Kilkenny", "Offaly", "Tipperary", "Waterford")) {
-  if(!dir.exists(file.path("maps", county)))
-    dir.create(file.path("maps", county))
+# dump townland data to CSV
+townlands.out <- st_drop_geometry(townlands)
+townlands.out <- townlands.out[, c("NAME_TAG", "NAME_GA", "NAME_EN", "ALT_NAME", "ALT_NAME_G", "ATTRIBUTIO", "CO_NAME", "CP_NAME", "ED_NAME", "BAR_NAME", "T_IE_URL")]
+write.csv(townlands.out, file.path("data", "townlands.csv"), fileEncoding = "UTF-8")
+
+dir.create("maps", showWarnings = FALSE)
+
+counties <- c("Kilkenny", "Offaly", "Tipperary", "Waterford")
+
+map_parish <- function(parish) {
+  townlands_in_parish <- rbind(townlands[townlands$CP_OSM_ID == parish$OSM_ID, ], townlands.lost[parish, ])
+  townlands_in_parish$BAR_NAME <- droplevels(townlands_in_parish$BAR_NAME)
   
-  county.shp <- townlands[townlands$CO_NAME==county, ]
-  county.shp$CP_NAME <- droplevels(county.shp$CP_NAME)
-  for (parish in levels(county.shp$CP_NAME)) {
-    parish.shp <- county.shp[county.shp$CP_NAME==parish, ]
-    parish.shp$NAME_TAG <- droplevels(parish.shp$NAME_TAG)
-    parish.map <- tm_shape(parish.shp)
-    parish.map <- parish.map + tm_polygons("MAP_COLORS", ncols = 5)
-    parish.map <- parish.map + tm_text("NAME_TAG") # auto.placement = TRUE
-    parish.map <- parish.map + tm_legend(legend.show = FALSE)
-    parish.map <- parish.map + tm_scale_bar()
-    #parish.map <- parish.map + tm_compass()
-    tmap_save(parish.map, file.path("maps", county, paste0(parish, ".pdf")), width=10, height=10, units = "in")
-  }
+  outdir <- file.path("maps", parish$CO_NAMES, paste(levels(townlands_in_parish$BAR_NAME), collapse=", "))
+  dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
+
+  # layer 0: parish
+  parish.map <- tm_shape(parish)
+  parish.map <- parish.map + tm_fill()
+  # layer 1: townlands
+  parish.map <- parish.map + tm_shape(townlands_in_parish)
+  parish.map <- parish.map + tm_polygons("MAP_COLORS", ncol = 5)
+  # parish.map <- parish.map + tm_legend(legend.show = FALSE)
+  # layer 2: parish outline and scale
+  parish.map <- parish.map + tm_shape(parish)
+  parish.map <- parish.map + tm_borders("red")
+  parish.map <- parish.map + tm_scale_bar()
+  # layer 3: townland names
+  parish.map <- parish.map + tm_shape(townlands_in_parish)
+  parish.map <- parish.map + tm_text("NAME_TAG") # auto.placement = TRUE
+  tmap_save(parish.map, file.path(outdir, paste0(parish$NAME_TAG, ".pdf")), width = 10, height = 10, units = "in")
 }
+
+# poor little lost townlands without a parish...
+townlands.lost <- townlands[is.na(townlands$CP_OSM_ID), ]
+
+parishes.filtered <- filter(parishes, grepl(paste(counties, collapse = "|"), CO_NAMES))
+parishes.filtered$NAME_TAG <- droplevels(parishes.filtered$NAME_TAG)
+by(parishes.filtered, parishes.filtered$OSM_ID, map_parish)
